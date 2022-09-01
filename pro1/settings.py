cf3633059eec1476e4c0c6639d33c719678edf20
Ankit Maniya <ankit.maniya6588@gmail.com>
2022-09-01 12:59:14 +0530

    fix django secret key expose!

--- pro1/settings.py
+++ pro1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+=-3(t@-=5di)k56!4=c%$2hzj6yu6tjaaz35pi6eckcd7q%zm'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
