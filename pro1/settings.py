21999602833c55ddd7e8e142bcaf11612879aa6c
Ankit Maniya <ankit.maniya6588@gmail.com>
2022-09-09 12:49:00 +0530

    added restriction in watching json view

--- pro1/settings.py
+++ pro1/settings.py
@@ -142,4 +142,7 @@ REST_FRAMEWORK = {
     },
     # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PAGINATION_CLASS': 'apps.core.pagination.StandardResultsSetPagination'
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
 }
